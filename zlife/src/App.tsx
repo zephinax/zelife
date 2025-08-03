62171d978ca73b83d121250a962993f33472f35b
Zephinax <zephinax@gmail.com>
2025-08-03 21:43:49 +0330

    add compose

--- zlife/src/App.tsx
+++ zlife/src/App.tsx
@@ -3,9 +3,10 @@ import { useTheme } from "./hooks/useTheme";
 import { useFinanceStore } from "./store/store";
 import Auth from "./pages/initialPages/Auth";
 import { useEffect } from "react";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
-  const { createYear, removeYear, userName, language } = useFinanceStore();
+  const { userName, language } = useFinanceStore();
   useEffect(() => {
     if (language === "fa") {
       document.body.setAttribute("dir", "rtl");
@@ -19,33 +20,7 @@ function App() {
       <Routes>
         {userName ? (
           <>
-            <Route
-              path="/"
-              element={
-                <div>
-                  <div className="bg-background text-content p-6">
-                    <button
-                      onClick={() => createYear("1404")}
-                      className="bg-primary text-white px-4 py-2 rounded m-2"
-                    >
-                      create year
-                    </button>
-                    <button
-                      onClick={() => removeYear("1404")}
-                      className="bg-tertiary text-white px-4 py-2 rounded m-2"
-                    >
-                      delete year
-                    </button>
-                    <div>
-                      {window.matchMedia("(prefers-color-scheme: dark)").matches
-                        ? "dark"
-                        : "light"}
-                    </div>
-                  </div>
-                  <div className="bg-background text-content min-h-screen p-6 space-y-4"></div>
-                </div>
-              }
-            />
+            <Route path="/" element={<Dashboard />} />
           </>
         ) : (
           <>
---
+++ zlife/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+  return <div className="w-screen min-h-screen">Dashboard</div>;
+}
