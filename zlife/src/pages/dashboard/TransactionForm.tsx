db163c3c2215b80e021438347ccea1259a41b5d9
Zephinax <zephinax@gmail.com>
2025-08-04 13:37:36 +0330

    add transactin type

--- zlife/src/pages/dashboard/TransactionForm.tsx
+++ zlife/src/pages/dashboard/TransactionForm.tsx
@@ -1,18 +1,37 @@
 import { useForm } from "react-hook-form";
 import SelectBox from "../../components/inputs/SelectBox";
 import { useTranslation } from "../../hooks/useTranslation";
+import Button from "../../components/button/Button";
 
 export default function TransactionForm() {
   const transactionOptions = ["income", "expense"];
   const { t } = useTranslation();
   const transactionForm = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = transactionForm;
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log(data);
+      })}
+      className="pb-2 flex flex-col gap-4"
+    >
       <SelectBox
         label={t("transaction.transactionType")}
         placeholder={t("transaction.choseOption")}
         options={transactionOptions}
+        {...register("type", {
+          required: `${t("transaction.transactionTypeRequired")}`,
+        })}
+        errorText={errors.type?.message as string}
+        onChange={() => {}}
       />
+      <Button className="w-full" type="submit">
+        {t("dashboard.addTransaction")}
+      </Button>
     </form>
   );
 }
